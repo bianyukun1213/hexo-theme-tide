f7e0fcedcf1ac0d80501d2dcc8b0b6b14b61b620
Hollis <bianyukun1213@outlook.com>
2024-09-25 08:52:14 +0800

    制作归档页面

--- scripts/tide-helper-meta-provider.js
+++ scripts/tide-helper-meta-provider.js
@@ -19,16 +19,24 @@ const {
     camelCaseObjToUnderScoreCaseObj
 } = require('./tide-utils.js');
 
-const scriptName = 'tide-helper-config-parser';
+let metaCache = {};
+
+const scriptName = 'tide-helper-meta-provider';
 // 合并站点、主题、页面和默认的配置，只检查空值不检查类型。
-hexo.extend.helper.register('parse_config', function (site, config, theme, page) {
+hexo.extend.helper.register('get_meta', function (site, config, theme, page) {
+    if (isPlainObject(metaCache[page.path]))
+        return metaCache[page.path];
     let out = {};
     out.site_title = config?.title ?? '';
     out.site_subtitle = config?.siteSubtitle ?? '';
     out.description = page?.description ?? config?.description ?? '';
     out.keywords = page?.keywords ?? config?.keywords ?? [];
-    out.author = page?.author ?? config?.author ?? '';
+    out.site_author = config?.author ?? '';
     out.language = page?.language ?? page?.lang ?? config?.language;
+    out.index_per_page = page?.per_page ?? config?.per_page ?? 10;
+    out.index_total = page?.total ?? 0;
+    out.index_current = page?.current ?? 0;
+    out.index_current_url = page?.current_url ?? '';
     // 上为站点配置，下为主题配置。
     out.direction = theme.direction === 'rtl' ? 'rtl' : 'ltr';
     if (isString(page.direction))
@@ -85,7 +93,12 @@ hexo.extend.helper.register('parse_config', function (site, config, theme, page)
 
     // 上为主题配置，下为页面配置。
     out.page_title = page?.title ?? ''; // 有些内置页面此项为空，需单独处理。
-
+    out.page_author = page?.author ?? config?.author ?? '';
+    out.path = page?.path ?? '';
+    out.permalink = page?.permalink ?? '';
+    out.date = page?.date ?? '';
+    out.updated = page?.updated ?? '';
+    out.excerpt = page?.excerpt ?? '';
     // out.languages = site?.data?.language_map[page.path] ?? {};
     // if (page.languages)
     //     out.languages = page.languages;
@@ -94,8 +107,31 @@ hexo.extend.helper.register('parse_config', function (site, config, theme, page)
         out.languages = page.languages;
     else if (site?.data?.language_map)
         out.languages = site.data.language_map[page.path]?.languages ?? {};
-    // out.languages = site?.data?.language_map;
-
+    out.page_categories = [];
+    if (page?.categories?.length) {
+        page.categories.forEach((cate) => {
+            out.page_categories.push({
+                name: cate.name,
+                id: cate._id,
+                parent: cate.parent ?? '',
+                slug: cate.slug,
+                path: cate.path,
+                permalink: cate.permalink
+            });
+        });
+    }
+    out.page_tags = [];
+    if (page?.tags?.length) {
+        page.tags.forEach((tag) => {
+            out.page_tags.push({
+                name: tag.name,
+                id: tag._id,
+                slug: tag.slug,
+                path: tag.path,
+                permalink: tag.permalink
+            });
+        });
+    }
     out.copyright = theme?.copyright?.default ?? ''; // 主题配置中只是预置键值，具体还要看页面，所以认为是页面配置。
     if (page.copyright)
         if (theme.copyright && page.copyright in theme.copyright)
@@ -143,5 +179,6 @@ hexo.extend.helper.register('parse_config', function (site, config, theme, page)
     out.page_encrypted = false;
     if ('encrypt' in config && 'password' in page)
         out.page_encrypted = true;
+    metaCache[page.path] = out;
     return out;
 }); // todo：优先级设为可配置项。
