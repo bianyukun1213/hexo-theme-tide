169b95661b30322e984a56e3996532366b840a6e
Hollis <bianyukun1213@outlook.com>
2025-02-10 22:52:12 +0800

    update

--- scripts/tide-filter-image-handler.js
+++ scripts/tide-filter-image-handler.js
@@ -6,22 +6,14 @@ hexo.extend.filter.register('after_post_render', function (data) {
     const classFigure = 'tide-image-figure';
     const classCaption = 'tide-image-caption';
     if (data.layout === 'post' || data.layout === 'page') {
-
-console.log(data.path)
-
         // 含 title，添加 figure 和 figcaption。
         data.content = data.content.replace(/<p\b[^>]*>\s*(<img\b[^>]*\btitle="([^"]+)"[^>]*>)<\/p>/g, (match, group1, group2) => {
-            console.log('title group1',group1)
-            console.log('title group2',group2)
             const modifiedGroup1 = group1.replace('<img', '<img tabindex="0"');
-            console.log('title modified group1',modifiedGroup1)
             return `<figure class="${classFigure}">${modifiedGroup1}<figcaption class="${classCaption}">${group2}</figcaption></figure>`;
         });
         // 不含，只添加 figure。
         data.content = data.content.replace(/<p\b[^>]*>\s*(<img\b(?:(?!\btitle=)[^>])*>)<\/p>/g, (match, group1) => {
-            console.log('notitle group1',group1)
             const modifiedGroup1 = group1.replace('<img', '<img tabindex="0"');
-            console.log('notitle modified group1', modifiedGroup1)
             return `<figure class="${classFigure}">${modifiedGroup1}</figure>`;
         });
     }
