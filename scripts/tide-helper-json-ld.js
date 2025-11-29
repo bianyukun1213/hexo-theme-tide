0af5a146f5df780869c88b0be690129682c0aa0d
Hollis <bianyukun1213@outlook.com>
2025-11-29 14:40:26 +0800

    add keywords to JSON-LD

--- scripts/tide-helper-json-ld.js
+++ scripts/tide-helper-json-ld.js
@@ -3,13 +3,14 @@
 const { isString, isArray } = require('./tide-utils.js');
 
 const scriptName = 'tide-helper-json-ld';
-hexo.extend.helper.register('json_ld', function (jsonldConfig, layout, language, headline, description, author, datePublished, dateModified, mainEntityOfPage, image) {
+hexo.extend.helper.register('json_ld', function (jsonldConfig, layout, language, headline, description, keywords, author, datePublished, dateModified, mainEntityOfPage, image) {
     let jsonldTemplate = {
         "@context": 'https://schema.org',
         "@type": (layout === 'post' || layout === 'page') ? 'BlogPosting' : 'Article',
         "@language": language,
         "headline": headline,
         "description": description,
+        "keywords": isArray(keywords) ? keywords.join('') : keywords,
         "datePublished": datePublished.format(),
         "dateModified": dateModified.format(),
         "mainEntityOfPage": mainEntityOfPage
